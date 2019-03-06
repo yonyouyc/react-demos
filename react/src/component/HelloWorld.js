85488f89aa375133b39a7c175b99325201aaea40
zhangwjr <zhangwjr@yonyou.com>
2019-03-06 17:32:08 +0800

    helloWorld

---
+++ react/src/component/HelloWorld.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class HelloWorld extends React.Component {
+    render(){
+        return (
+            <div>
+                <h1>HelloWorld</h1>
+            </div>
+        )
+    }
+}
+
+export default HelloWorld;
